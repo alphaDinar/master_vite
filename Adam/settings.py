131c94d69d3013a9b5f83d168857ed85b444cfe9
alphaDinar <yanksone18@yahoo.com>
2023-03-27 10:42:47 +0000

    change

--- Adam/settings.py
+++ Adam/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,13 +67,16 @@ WSGI_APPLICATION = 'Adam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://master_db_j3ul_user:ZCtloKRgfyOeDNfM0TpNa7pDYLpsgQeL@dpg-cggn3mm4daddcg4r447g-a.oregon-postgres.render.com/master_db_j3ul')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Adam.wsgi --log-file -
